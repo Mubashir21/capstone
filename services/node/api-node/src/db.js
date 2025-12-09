2746ed5eb098efe485c8a0954b3829ce4d95d0a9
Mubashir <mubashirshoukat@gmail.com>
2025-12-09 11:25:58 +0300

    test again

--- services/node/api-node/src/db.js
+++ services/node/api-node/src/db.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 });
 
 // the pool will emit an error on behalf of any idle client
-// it contains if a backend error or network partition happen
+// it contains if a backend error or network partition happens
 pool.on("error", (err, client) => {
   console.error("Unexpected error on idle client", err);
   process.exit(-1);
